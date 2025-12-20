f4e415f1c308797297c63ed86ed2403e371d55d7
Pham Nguyen Gia Huy <huykunfu.376@gmail.com>
2025-12-20 09:47:08 +0700

    finalize web

--- web/backend/app/ml/models/model.py
+++ web/backend/app/ml/models/model.py
@@ -1,4 +1,4 @@
-import joblib
+import pickle
 from sentence_transformers import SentenceTransformer
 from app.core.config import settings
 from app.ml.preprocess.preprocessing import preprocess
@@ -11,18 +11,29 @@ class CleanTalk1:
             settings.EMBEDDING_MODEL_NAME,
             cache_folder=str(settings.EMBEDDING_CACHE_DIR),
         )
-        model_obj = joblib.load(settings.TOXICITY_MODEL_PATH)
-        self.svm_model = model_obj["svm"]
+        with open(settings.TOXICITY_MODEL_PATH, "rb") as f:
+            obj = pickle.load(f)
+
+        self.svm_model = obj["svm"]
+        self.normalizer = obj.get("normalizer", None)
 
     def predict(self, text: str) -> dict:
         clean = preprocess(text)
-        emb = self.encoder.encode([clean])
+        emb = self.encoder.encode(
+            [clean], convert_to_numpy=True, normalize_embeddings=True
+        )
+
+        if self.normalizer is not None:
+            emb = self.normalizer.transform(emb)
         y_pred = self.svm_model.predict(emb)[0]
-        points = compute_points(y_pred)
-        final_label = classify(points, y_pred)
+        y_pred_int = [int(x) for x in y_pred]
+
+        points = compute_points(y_pred_int)
+        final_label = classify(points, y_pred_int)
+
         return {
             "clean_text": clean,
-            "labels": {LABELS[i]: int(y_pred[i]) for i in range(len(LABELS))},
+            "labels": {LABELS[i]: y_pred_int[i] for i in range(len(LABELS))},
             "points": points,
             "final_label": final_label,
         }
